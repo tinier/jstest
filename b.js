3146bf70317bcf47328770b990b0cdcdf1380267
tinier <tinierrokoo@gmail.com>
2017-03-07 23:23:03 +0800

    Update b.js

--- b.js
+++ b.js
@@ -11,11 +11,25 @@ function mult(m) {
 }
 
 console.log(mult.call(x, 20));
+
 console.log(mult.apply(y, [10]));
 
 
 console.log(mult(32));
+
 var n = 800;
 console.log(mult(32));
+
 var n = 80;
 console.log(mult(32));
+
+
+Function.prototype.bind = function(obj) {
+  var method = this;
+  var f = function() {
+    return method.apply(obj, arguments);
+  }
+  return f;
+};
+
+console.log(mult.bind(y)(99));
